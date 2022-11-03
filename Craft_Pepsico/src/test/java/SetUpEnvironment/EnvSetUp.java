791f02b7da44a5a1f1e08d43345dc8749e080e30
Prasad <prasad.patil.parijat@gmail.com>
2022-11-03 23:05:38 +0530

    Envirnment Setup

--- Craft_Pepsico/src/test/java/SetUpEnvironment/EnvSetUp.java
+++ Craft_Pepsico/src/test/java/SetUpEnvironment/EnvSetUp.java
@@ -1,4 +1,20 @@
 package SetUpEnvironment;
 
-public class EnvSetUp {
+public class EnvSetUp
+{
+    public Properties property = Settings.getInstance();
+
+    public EnvSetUp()
+    {
+        if(System.getProerty("env").equalsIgnoreCase("qa"))
+        {
+            property.setProperty("GetProductsUrl","https://api-na.qa.mypepsico.com/it/och/b2bproductsvc/V1/api/products");
+        }
+        else if (System.getProperty("env").equalsIgnoreCase("dev"))
+        {
+            property.setProperty("GetProductsUrl","https://api-na.dev.mypepsico.com/it/och/b2bproductsvc/V1/api/products")
+        }
+
+        System.out.println(property.getProperty("GetProductsUrl"));
+    }
 }
