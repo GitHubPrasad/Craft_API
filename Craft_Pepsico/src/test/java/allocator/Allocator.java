34e85f4c2e709189b04111882deb4f4cd5dc15f6
Prasad <prasad.patil.parijat@gmail.com>
2022-11-03 10:21:52 +0530

    Added all dependencies in pom.xml

--- Craft_Pepsico/src/test/java/allocator/Allocator.java
+++ Craft_Pepsico/src/test/java/allocator/Allocator.java
@@ -1,4 +1,52 @@
 package allocator;
 
-public class Allocator {
+import com.sun.scenario.Settings;
+
+import java.util.Properties;
+
+public class Allocator
+{
+    /*
+        Class to manage batch execution of test scripts within the framework
+     */
+    private FrameworkParameters frameworkParameters = FrameworkParameters.getInstance();
+    private Properties properties;
+    private Properties mobileproperties;
+    private ResultSummaryManager resultSummaryManager = ResultSummaryManager.getInstance();
+
+    private static ExtentHtmlReporter htmlReporter;
+    private static ExtentReports extentReport;
+    private static ExtentTest extentTest;
+    private static KlovReporter klovReporter = new KlovReporter;
+
+    /*
+        The entry point of test batch execution <br>
+        Exists with a value of 0 if the test passes and 1 if the test fails
+        @param args
+        command line arguments to the Allocator (Not Applicable)
+     */
+    public static void main(String[] args)
+    {
+        Allocator allocator = new Allocator();
+        allocator.driveBatchExecution();
+    }
+
+    private void driveBatchExecution()
+    {
+        resultSummaryManager.setRelativePath();
+        properties = Settings.getInstance();
+        System.out.println("properties::"+properties);
+        mobileproperties = Settings.getMobilePropertiesInstance();
+        String runConfiguration;
+
+    }
+
+
+
+
+
+
+
+
+
 }
